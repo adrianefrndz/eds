b36743d43beeb4fa994837de3b7a69f4ecfd173a
adrianefrndz <adrianefrndz07@gmail.com>
2025-12-24 09:24:40 +0800

    remove unused elements

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import Navbar from "./components/layout/Navbar";
 import Hero from "./components/sections/Hero";
 import Skills from "./components/sections/Skills";
 import Projects from "./components/sections/Projects";
-import CTA from "./components/sections/CTA";
 import Footer from "./components/layout/Footer";
 import Certificates from "./components/sections/Certificates";
 import Contact from "./components/sections/Contact";
@@ -15,7 +14,6 @@ function App() {
       <Certificates />
       <Skills />
       <Projects />
-      {/* <CTA /> */}
       <Contact />
       <Footer />
     </>
